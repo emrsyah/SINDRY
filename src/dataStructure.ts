d23480f4772d46f180b4ac251bfa7460ab99b714
Emirsyah <muhammademir48@gmail.com>
2023-02-10 11:07:03 +0700

    transactions page

--- src/dataStructure.ts
+++ src/dataStructure.ts
@@ -1,32 +1,53 @@
 export interface Outlet {
-    id: number;
-    name: string;
-    address: string;
-    contact: string;
-    created_at: Date;
+  id: number;
+  name: string;
+  address: string;
+  contact: string;
+  created_at: Date;
 }
 
-export type OutletListType = Outlet[]
+export type OutletListType = Outlet[];
 export interface Customer {
-    id: number;
-    name: string;
-    address: string;
-    gender: "L" | "P";
-    contact: string;
-    outlet_id: number;
-    created_at: Date;
+  id: number;
+  name: string;
+  address: string;
+  gender: "L" | "P";
+  contact: string;
+  outlet_id: number;
+  created_at: Date;
 }
 
-export type CustomerListType = Customer[]
+export type CustomerListType = Customer[];
 
 export interface Product {
-    id: number;
-    outlet_id: number;
-    type: string;
-    name: string;
-    price: number;
-    created_at: Date;
-    outlet_name: string;
+  id: number;
+  outlet_id: number;
+  type: string;
+  name: string;
+  price: number;
+  created_at: Date;
+  outlet_name: string;
 }
 
-export type ProductListType = Product[]
+export type ProductListType = Product[];
+
+export interface Transaction {
+  id: number;
+  customer_id: number;
+  created_at: Date;
+  total: number;
+  cashier_id: number;
+  invoice_code: string;
+  outlet_id: number;
+  additional_cost: number;
+  discount: number;
+  taxes: number;
+  status: "new" | "on_process" | "finished" | "picked_up";
+  is_paid: number;
+  deadline: Date;
+  paid_at: Date;
+  outlet_name: string;
+  cashier_name: string;
+}
+
+export type TransactionListType = Transaction[];
--- src/pages/app/admin/Orderan.tsx
+++ src/pages/app/admin/Orderan.tsx
@@ -1,9 +1,143 @@
-import React from 'react'
+import React, { useEffect, useMemo } from "react";
+import "@/styles/adminGeneral.scss";
+import { connectionSql } from "@/sqlConnect";
+import { useState } from "react";
+import dayjs from "dayjs";
+import Table from "@/components/Table";
+import EmptyTable from "@/components/EmptyTable";
+import { TransactionListType } from "../../../dataStructure";
+import rupiahConverter from "../../../helpers/rupiahConverter";
+import { UilCheckCircle, UilTimesCircle  } from '@iconscout/react-unicons'
 
 const Orderan = () => {
-  return (
-    <div>Orderan</div>
-  )
-}
+  const [transactions, setTransactions] = useState<TransactionListType>([]);
+  const [filterInput, setFilterInput] = useState<string>("");
 
-export default Orderan
+  const handleFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value || "";
+    setFilterInput(value);
+  };
+
+  useEffect(() => {
+    connectionSql.connect();
+    var stateSql =
+      "SELECT products.*, outlets.name AS outlet_name FROM products JOIN outlets ON outlets.id = products.outlet_id";
+    var tmpSql =
+      "SELECT t.*, o.name AS outlet_name, u.name AS cashier_name, c.name AS customer_name FROM transactions t, outlets o, users u, customers c WHERE t.outlet_id = o.id AND t.cashier_id = u.id AND t.customer_id = c.id";
+    connectionSql.query(tmpSql, (err, results, fields) => {
+      if (err) console.error(err);
+      else {
+        // console.log(results);
+        // setProducts(results);
+        setTransactions(results);
+      }
+    });
+  }, []);
+
+  const dataMemo = useMemo(() => transactions, [transactions]);
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Id",
+        accessor: "id",
+        Cell: ({ cell: { value } }: { cell: { value: number } }) => (
+          <>#{value}</>
+        ),
+      },
+      {
+        Header: "Tanggal",
+        accessor: "created_at",
+        Cell: ({ cell: { value } }: { cell: { value: Date } }) => (
+          <>{dayjs(value).format("DD MMM")}</>
+        ),
+        // Cell: ({ cell: { value } }: { cell: { value: "kiloan" | "bed_cover" | "selimut" | "kaos" | "lainnya" } }) => (
+        //   <span className={`${value === "bed_cover" ? "bcType" : value === "kaos" ? "kType" : value === "kiloan" ? "klType" : value === "selimut" ? "sType" : "lType"}`}>{(value)}</span>
+        // ),
+      },
+      {
+        Header: "Status",
+        accessor: "status",
+        Cell: ({ cell: { value } }: { cell: { value: string } }) => (
+          <span
+            className={`${
+              value === "new"
+                ? "lType"
+                : value === "on_process"
+                ? "kType"
+                : value === "finished"
+                ? "sType"
+                : value === "picked_up"
+                ? "bcType"
+                : ""
+            }`}
+          >
+            {value}
+          </span>
+        ),
+      },
+      {
+        Header: "Pelanggan",
+        accessor: "customer_name",
+        Cell: ({ cell: { value } }: { cell: { value: string } }) => (
+          <span className="">{value}</span>
+        ),
+      },
+      {
+        Header: "Bayar",
+        accessor: "is_paid",
+        Cell: ({ cell: { value } }: { cell: { value: number } }) => (
+          <span className="paidStatus">
+            {value === 1 ? (
+              <>
+                <UilCheckCircle size="18" className="cIcon" />
+                <p>Dibayar</p>
+              </>
+            ) : (
+              <>
+              <UilTimesCircle size="18" className="xIcon" />
+              <p>Belum</p>
+              </>
+            )}
+          </span>
+        ),
+      },
+      {
+        Header: "Total",
+        accessor: "total",
+        Cell: ({ cell: { value } }: { cell: { value: number } }) => (
+          <span className="">{rupiahConverter(value)}</span>
+        ),
+      },
+    ],
+    []
+  );
+
+  return (
+    <div>
+      <div className="outletTitle">
+        <h2>Orderan</h2>
+        <button>Tambah Baru</button>
+      </div>
+      <div className="filterInput">
+        <input
+          type="text"
+          placeholder="Cari Dengan Nama"
+          onChange={handleFilterChange}
+          value={filterInput}
+        />
+      </div>
+      {dataMemo.length ? (
+        <Table
+          columns={columns}
+          data={dataMemo}
+          filterColumn="id"
+          filterInput={filterInput}
+        />
+      ) : (
+        <EmptyTable columns={columns} />
+      )}
+    </div>
+  );
+};
+
+export default Orderan;
